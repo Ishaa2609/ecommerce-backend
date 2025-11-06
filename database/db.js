e516a56ad41513601939cca87d43802b135137c8
Ishaa2609 <ishaofficalwork@gmail.com>
2025-11-06 20:07:36 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+
+    // const URL = `mongodb+srv://${username}:${password}@cluster0.ilmdtbf.mongodb.net/`
+    const URL = `mongodb+srv://${username}:${password}@cluster0.0cunwue.mongodb.net/`
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
